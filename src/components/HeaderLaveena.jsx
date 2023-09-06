2ab6167d8dcc83c0c21d257829a6f894d2be5d09
Umair Khalid <umair@zaviago.com>
2023-09-06 16:00:59 +0500

    laveena

--- src/components/HeaderLaveena.jsx
+++ src/components/HeaderLaveena.jsx
@@ -63,14 +63,14 @@ export default function Example() {
     <Popover className="relative bg-[#68825B]">
       <div className="flex items-center justify-between px-6 py-3 md:justify-start md:space-x-10">
         <div className="flex justify-start lg:w-0 lg:flex-1">
-          <a href="#">
+          <Link to="/">
             <span className="sr-only">Your Company</span>
             <img
               className="h-auto w-[120px]"
               src={LaveenaLogo}
               alt=""
             />
-          </a>
+          </Link>
         </div>
         <div className="-my-2 -mr-2 md:hidden">
           <Popover.Button className="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
