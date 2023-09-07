1ceb2d9384a3ad8c395b5271c56dfb8f9eacc6c3
Umair Khalid <umair@zaviago.com>
2023-09-07 14:55:46 +0500

    Update ProductDetailPage.jsx

--- src/pages/ProductDetailPage.jsx
+++ src/pages/ProductDetailPage.jsx
@@ -35,9 +35,9 @@ const reviews = {
       content: `
           <p>This icon pack is just what I need for my latest project. There's an icon for just about anything I could ever need. Love the playful look!</p>
         `,
-      date: "July 16, 2021",
+      date: "July 16, 2023",
       datetime: "2021-07-16",
-      author: "Emily Selman",
+      author: "John Doe",
       avatarSrc:
         "https://images.unsplash.com/photo-1502685104226-ee32379fefbe?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=256&h=256&q=80",
     }
