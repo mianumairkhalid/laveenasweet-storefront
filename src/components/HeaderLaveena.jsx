463a9c86051791cc3fb91df96311c24cce098db3
Umair Khalid <umair@zaviago.com>
2023-09-08 10:07:10 +0500

    laveena

--- src/components/HeaderLaveena.jsx
+++ src/components/HeaderLaveena.jsx
@@ -1,8 +1,9 @@
-import { Fragment } from "react";
 import { Link } from "react-router-dom";
 import LaveenaLogo from "../img/Laveena-Logo.png";
-import { Popover, Transition } from "@headlessui/react";
+import { Popover, Transition, Dialog, Tab } from "@headlessui/react";
 import circleProfile from "../img/circle-profile.png";
+import { Fragment, useRef, useState } from "react";
+import { ExclamationTriangleIcon } from "@heroicons/react/24/outline";
 import {
   ArrowPathIcon,
   Bars3Icon,
@@ -81,8 +82,253 @@ function classNames(...classes) {
 }
 
 export default function Example() {
+  const [open, setOpen] = useState(false);
+
+  const cancelButtonRef = useRef(null);
+
   return (
     <Popover className="relative bg-[#68825B]">
+      <Transition.Root show={open} as={Fragment}>
+        <Dialog
+          as="div"
+          className="relative z-10"
+          initialFocus={cancelButtonRef}
+          onClose={setOpen}
+        >
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+          </Transition.Child>
+
+          <div className="fixed inset-0 z-10 overflow-y-auto">
+            <div className="flex min-h-full items-center justify-center p-4 text-center sm:items-center sm:p-0">
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                enterTo="opacity-100 translate-y-0 sm:scale-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+                leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+              >
+                <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white pb-4 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+                  <div className="">
+                    <div className="mx-auto lg:col-span-4 lg:mt-0 lg:max-w-none">
+                      <Tab.Group as="div">
+                        <div className="border-b border-gray-200">
+                          <Tab.List className="-mb-px justify-center flex space-x-8">
+                            <Tab
+                              className={({ selected }) =>
+                                classNames(
+                                  selected
+                                    ? "border-black bg-[#FAFAFA]"
+                                    : "border-transparent",
+                                  "whitespace-nowrap text-[12px] font-semibold text-[#383838] border-b-2 outline-none py-6 w-1/2"
+                                )
+                              }
+                            >
+                              SIGN IN
+                            </Tab>
+                            <Tab
+                              className={({ selected }) =>
+                                classNames(
+                                  selected
+                                    ? "border-black bg-[#FAFAFA]"
+                                    : "border-transparent",
+                                  "whitespace-nowrap text-[12px] font-semibold text-[#383838] border-b-2 outline-none py-6  w-1/2"
+                                )
+                              }
+                            >
+                              SING UP
+                            </Tab>
+                          </Tab.List>
+                        </div>
+                        <Tab.Panels as={Fragment}>
+                          <Tab.Panel className="mb-5 pt-[30px] lg:pt-[60px] px-[40px] lg:px-[70px]">
+                            <h3 className="text-[20px] font-bold leading-[30px] mb-10 mt-5">
+                              <form className="space-y-8 ">
+                                <div className="space-y-8">
+                                  <div className="">
+                                    <div className="mt-6 grid grid-cols-1 gap-y-6 gap-x-4 sm:grid-cols-6">
+                                      <div className="sm:col-span-full">
+                                        <label
+                                          htmlFor="email"
+                                          className="block text-sm font-normal text-[#383838]"
+                                        >
+                                          Username or Email Address
+                                        </label>
+                                        <div className="mt-1">
+                                          <input
+                                            id="useremail"
+                                            name="useremail"
+                                            type="text"
+                                            autoComplete="useremail"
+                                            className="bg-[#ECECEC] h-[40px] block w-full rounded-md border focus:border-[#ECECEC] sm:text-sm"
+                                          />
+                                        </div>
+                                      </div>
+
+                                      <div className="sm:col-span-full">
+                                        <label
+                                          htmlFor="email"
+                                          className="block text-sm  font-normal text-[#383838]"
+                                        >
+                                          Password
+                                        </label>
+                                        <div className="mt-1">
+                                          <input
+                                            id="password"
+                                            name="password"
+                                            type="password"
+                                            autoComplete="password"
+                                            className="bg-[#ECECEC] h-[40px] block w-full rounded-md border focus:border-[#ECECEC] sm:text-sm"
+                                          />
+                                        </div>
+                                      </div>
+                                    </div>
+                                  </div>
+
+                                  <div className="mt-[5px]">
+                                    <div className="mt-3">
+                                      <fieldset>
+                                        <div className="space-y-4">
+                                          <div className="relative flex items-start">
+                                            <div className="flex h-5 items-center">
+                                              <input
+                                                id="rememberme"
+                                                name="rememberme"
+                                                type="checkbox"
+                                                className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
+                                              />
+                                            </div>
+                                            <div className="ml-3 text-sm">
+                                              <label
+                                                htmlFor="rememberme"
+                                                className="font-normal text-[#383838]"
+                                              >
+                                                Remember Me
+                                              </label>
+                                            </div>
+                                          </div>
+                                        </div>
+                                      </fieldset>
+                                    </div>
+                                  </div>
+                                </div>
+
+                                <div className="pt-2">
+                                  <button
+                                    type="submit"
+                                    className="w-full rounded-sm border border-transparent bg-[#D5828B] py-2 mx-0 text-sm font-medium text-white hover:bg-black focus:outline-none "
+                                  >
+                                    Login
+                                  </button>
+                                </div>
+                              </form>
+                            </h3>
+                          </Tab.Panel>
+
+                          <Tab.Panel className="pt-[30px] lg:pt-[60px] px-[40px] lg:px-[70px]">
+                            <div className="">
+                              <div className="">
+                                <h3 className="text-[20px] font-bold leading-[30px] mb-10 mt-5">
+                                <form className="space-y-8 ">
+                                <div className="space-y-8">
+                                  <div className="">
+                                    <div className="mt-6 grid grid-cols-1 gap-y-6 gap-x-4 sm:grid-cols-6">
+                                      <div className="sm:col-span-full">
+                                        <label
+                                          htmlFor="email"
+                                          className="block text-sm font-normal text-[#383838]"
+                                        >
+                                          Username
+                                        </label>
+                                        <div className="mt-1">
+                                          <input
+                                            id="username"
+                                            name="username"
+                                            type="username"
+                                            autoComplete="username"
+                                            className="bg-[#ECECEC] h-[40px] block w-full rounded-md border focus:border-[#ECECEC] sm:text-sm"
+                                          />
+                                        </div>
+                                      </div>
+
+                                      <div className="sm:col-span-full">
+                                        <label
+                                          htmlFor="email"
+                                          className="block text-sm font-normal text-[#383838]"
+                                        >
+                                          Email Address
+                                        </label>
+                                        <div className="mt-1">
+                                          <input
+                                            id="email"
+                                            name="email"
+                                            type="email"
+                                            autoComplete="email"
+                                            className="bg-[#ECECEC] h-[40px] block w-full rounded-md border focus:border-[#ECECEC] sm:text-sm"
+                                          />
+                                        </div>
+                                      </div>
+
+                                      <div className="sm:col-span-full">
+                                        <label
+                                          htmlFor="email"
+                                          className="block text-sm  font-normal text-[#383838]"
+                                        >
+                                          Password
+                                        </label>
+                                        <div className="mt-1">
+                                          <input
+                                            id="password"
+                                            name="password"
+                                            type="password"
+                                            autoComplete="password"
+                                            className="bg-[#ECECEC] h-[40px] block w-full rounded-md border focus:border-[#ECECEC] sm:text-sm"
+                                          />
+                                        </div>
+                                      </div>
+                                    </div>
+                                  </div>
+
+                                  <div>
+                                    <p className="text-[#383838] text-sm font-normal leading-[23px]">Your personal data will be used to support your experience throughout this website, to manage access to your account, and for other purposes described in our privacy policy.</p>
+                                  </div>
+
+                                  
+                                </div>
+
+                                <div className="mt-1">
+                                  <button
+                                    type="submit"
+                                    className="w-full rounded-sm border border-transparent bg-[#D5828B] py-2 mx-0 text-sm font-medium text-white hover:bg-black focus:outline-none "
+                                  >
+                                    Register
+                                  </button>
+                                </div>
+                              </form>
+                                </h3>
+                              </div>
+                            </div>
+                          </Tab.Panel>
+                        </Tab.Panels>
+                      </Tab.Group>
+                    </div>
+                  </div>
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </div>
+        </Dialog>
+      </Transition.Root>
       <div className="flex items-center justify-between px-6 py-3 md:justify-start md:space-x-10">
         <div className="flex justify-start lg:w-0 lg:flex-1">
           <Link to="/">
@@ -197,7 +443,12 @@ export default function Example() {
           </Link>
         </Popover.Group>
         <div className="hidden items-center justify-end md:flex md:flex-1 lg:w-0">
-          <img className="w-[80px] mr-4" src={circleProfile} alt="" />
+          <img
+            onClick={() => setOpen(true)}
+            className="cursor-pointer w-[80px] mr-4"
+            src={circleProfile}
+            alt=""
+          />
         </div>
       </div>
 
@@ -218,15 +469,20 @@ export default function Example() {
             <div>
               <ul className=" space-y-2 py-3">
                 <li className=" pb-2.5 pt-2.5">
-                  <span className="px-5 text-white">Home</span></li>
+                  <span className="px-5 text-white">Home</span>
+                </li>
                 <li className=" pb-2.5 pt-2.5">
-                  <span className="px-5 text-white">Product</span></li>
+                  <span className="px-5 text-white">Product</span>
+                </li>
                 <li className=" pb-2.5 pt-2.5">
-                  <span className="px-5 text-white">Blog</span></li>
+                  <span className="px-5 text-white">Blog</span>
+                </li>
                 <li className=" pb-2.5 pt-2.5">
-                  <span className="px-5 text-white">About Us</span> </li>
+                  <span className="px-5 text-white">About Us</span>{" "}
+                </li>
                 <li className=" pb-2.5 pt-2.5">
-                  <span className="px-5 text-white">Contact Us</span> </li>
+                  <span className="px-5 text-white">Contact Us</span>{" "}
+                </li>
               </ul>
             </div>
           </div>
