18487baf569780ed13ae37b3309cff559d43891b
MarkSuttirak <suttirak.ch@zaviago.com>
2023-09-13 17:26:51 +0700

    Optimise Header Code

--- src/components/HeaderLaveena.jsx
+++ src/components/HeaderLaveena.jsx
@@ -4,84 +4,34 @@ import { Popover, Transition, Dialog, Tab } from "@headlessui/react";
 import circleProfile from "../img/circle-profile.png";
 import { Fragment, useRef, useState } from "react";
 import { ExclamationTriangleIcon } from "@heroicons/react/24/outline";
-import {
-  ArrowPathIcon,
-  Bars3Icon,
-  ChartBarIcon,
-  CursorArrowRaysIcon,
-  DocumentChartBarIcon,
-  ShieldCheckIcon,
-  Squares2X2Icon,
-  XMarkIcon,
-} from "@heroicons/react/24/outline";
-import { ChevronDownIcon } from "@heroicons/react/20/solid";
-
-const solutions = [
-  {
-    name: "Analytics",
-    description:
-      "Get a better understanding of where your traffic is coming from.",
-    href: "#",
-  },
-  {
-    name: "Engagement",
-    description: "Speak directly to your customers in a more meaningful way.",
-    href: "#",
-  },
-  {
-    name: "Security",
-    description: "Your customers' data will be safe and secure.",
-    href: "#",
-  },
-  {
-    name: "Integrations",
-    description: "Connect with third-party tools that you're already using.",
-    href: "#",
-  },
-  {
-    name: "Automations",
-    description:
-      "Build strategic funnels that will drive your customers to convert",
-    href: "#",
-  },
-  {
-    name: "Reports",
-    description:
-      "Get detailed reports that will help you make more informed decisions ",
-    href: "#",
-  },
-];
-const resources = [
-  {
-    name: "Help Center",
-    description:
-      "Get all of your questions answered in our forums or contact support.",
-    href: "#",
-  },
-  {
-    name: "Guides",
-    description:
-      "Learn how to maximize our platform to get the most out of it.",
-    href: "#",
-  },
-  {
-    name: "Events",
-    description:
-      "See what meet-ups and other events we might be planning near you.",
-    href: "#",
-  },
-  {
-    name: "Security",
-    description: "Understand how we take your privacy seriously.",
-    href: "#",
-  },
-];
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
 }
 
 export default function Example() {
+  const links = [
+    {
+      text: 'Home',
+      href: '/'
+    },
+    {
+      text: 'Product',
+      href: '/product'
+    },
+    {
+      text: 'Blog',
+      href: '/blog'
+    },
+    {
+      text: 'About Us',
+      href: '/laveena-about-us'
+    },
+    {
+      text: 'Contact Us',
+      href: '/branch'
+    }
+  ]
   const [open, setOpen] = useState(false);
 
   const cancelButtonRef = useRef(null);
@@ -147,7 +97,7 @@ export default function Example() {
                                 )
                               }
                             >
-                              SING UP
+                              SIGN UP
                             </Tab>
                           </Tab.List>
                         </div>
@@ -364,135 +314,20 @@ export default function Example() {
             </svg>
           </Popover.Button>
         </div>
-        <Popover.Group as="nav" className="hidden space-x-16  lg:flex">
-          <Popover className="relative">
-            {({ open }) => (
-              <>
-                {/* <Popover.Button
-                  className={classNames(
-                    open ? "text-gray-900" : "text-gray-500",
-                    "group inline-flex items-center rounded-md text-white text-[22px] font-bold hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                  )}
-                >
-                  <span>Home</span>
-                </Popover.Button> */}
-                <Link
-            to="/"
-            className={`text-black text-[22px] hover:font-bold hover:text-white hover:white ${
-              location.pathname === "/"
-                ? "text-white font-bold"
-                : "font-light"
-            }`}
-          >
-            Home
-          </Link>
-
-                <Transition
-                  as={Fragment}
-                  enter="transition ease-out duration-200"
-                  enterFrom="opacity-0 translate-y-1"
-                  enterTo="opacity-100 translate-y-0"
-                  leave="transition ease-in duration-150"
-                  leaveFrom="opacity-100 translate-y-0"
-                  leaveTo="opacity-0 translate-y-1"
-                >
-                  <Popover.Panel className="absolute z-10 -ml-4 mt-3 w-screen max-w-md transform lg:left-1/2 lg:ml-0 lg:max-w-2xl lg:-translate-x-1/2">
-                    <div className="overflow-hidden rounded-lg shadow-lg ">
-                      <div className="relative grid gap-6 bg-white px-5 py-6 sm:gap-8 sm:p-8 lg:grid-cols-2">
-                        home
-                        {solutions.map((solution) => (
-                          <a
-                            key={solution.name}
-                            href={solution.href}
-                            className="-m-3 flex items-start rounded-lg p-3 hover:bg-gray-50"
-                          >
-                            <div className="flex h-10 w-10 flex-shrink-0 items-center justify-center rounded-md bg-indigo-500 text-white sm:h-12 sm:w-12">
-                              <solution.icon
-                                className="h-6 w-6"
-                                aria-hidden="true"
-                              />
-                            </div>
-                            <div className="ml-4">
-                              <p className="text-base font-medium text-gray-900">
-                                {solution.name}
-                              </p>
-                              <p className="mt-1 text-sm text-gray-500">
-                                {solution.description}
-                              </p>
-                            </div>
-                          </a>
-                        ))}
-                      </div>
-                      <div className="bg-gray-50 p-5 sm:p-8">
-                        <a
-                          href="#"
-                          className="-m-3 flow-root rounded-md p-3 hover:bg-gray-100"
-                        >
-                          <div className="flex items-center">
-                            <div className="text-base font-medium text-gray-900">
-                              Enterprise
-                            </div>
-                            <span className="ml-3 inline-flex items-center rounded-full bg-indigo-100 px-3 py-0.5 text-xs font-medium leading-5 text-indigo-800">
-                              New
-                            </span>
-                          </div>
-                          <p className="mt-1 text-sm text-gray-500">
-                            Empower your entire team with even more advanced
-                            tools.
-                          </p>
-                        </a>
-                      </div>
-                    </div>
-                  </Popover.Panel>
-                </Transition>
-              </>
-            )}
-          </Popover>
-
-          
-          <Link
-            to="/product"
-            className={`text-black text-[22px]  hover:font-bold hover:text-white hover:white ${
-              location.pathname === "/product"
-                ? "text-white font-bold"
-                : "font-light"
-            }`}
-          >
-            Product
-          </Link>
-          
-          <Link
-            to="/blog"
-            className={`text-black text-[22px] hover:font-bold hover:text-white hover:white ${
-              location.pathname === "/blog"
-                ? "text-white font-bold"
-                : "font-light"
-            }`}
-          >
-            Blog
-          </Link>
-
-          <Link
-            to="/laveena-about-us"
-            className={`text-black text-[22px]  hover:font-bold hover:text-white hover:white ${
-              location.pathname === "/laveena-about-us"
-                ? "text-white font-bold"
-                : "font-light"
-            }`}
-          >
-            About us
-          </Link>
-          <Link
-            to="/branch"
-            className={`text-black text-[22px]  hover:font-bold hover:text-white hover:white ${
-              location.pathname === "/branch"
-                ? "text-white font-bold"
-                : "font-light"
-            }`}
-          >
-            Contact Us
-          </Link>
-        </Popover.Group>
+        <nav className="hidden lg:block">
+          {links.map((link) => (
+            <Link
+              to={link.href}
+              className={`text-black text-[22px] hover:font-bold hover:text-white hover:white w-[150px] text-center inline-block ${
+                location.pathname === link.href
+                  ? "text-white font-bold"
+                  : "font-light"
+              }`}
+            >
+              {link.text}
+            </Link>
+          ))}
+        </nav>
         <div className="hidden items-center justify-end lg:flex md:flex-1 lg:w-0">
           <img
             onClick={() => setOpen(true)}
@@ -519,21 +354,13 @@ export default function Example() {
           <div className="divide-y-2 divide-gray-50 rounded-lg bg-[#68825B] ring-1 ring-black ring-opacity-5">
             <div>
               <ul className=" space-y-2 py-3">
-                <li className=" pb-2.5 pt-2.5">
-                  <span className="px-5 text-white">Home</span>
-                </li>
-                <li className=" pb-2.5 pt-2.5">
-                  <span className="px-5 text-white">Product</span>
-                </li>
-                <li className=" pb-2.5 pt-2.5">
-                  <span className="px-5 text-white">Blog</span>
-                </li>
-                <li className=" pb-2.5 pt-2.5">
-                  <span className="px-5 text-white">About Us</span>{" "}
-                </li>
-                <li className=" pb-2.5 pt-2.5">
-                  <span className="px-5 text-white">Contact Us</span>{" "}
-                </li>
+                {links.map((link) => (
+                  <li className=" pb-2.5 pt-2.5">
+                    <Link to={link.href}>
+                      <span className="px-5 text-white">{link.text}</span>
+                    </Link>
+                  </li>
+                ))}
               </ul>
             </div>
           </div>
